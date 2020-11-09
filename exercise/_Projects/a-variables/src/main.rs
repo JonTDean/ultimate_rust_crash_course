59e5de9e727ca5b17e6ee71fbae1443a363d9202
JonTDean <JonathanTomDean@Gmail.com>
2020-11-09 18:10:41 -0500

    Finished: a-variables, learned about pattern variable assignment

--- exercise/_Projects/a-variables/src/main.rs
+++ exercise/_Projects/a-variables/src/main.rs
@@ -1,3 +1,26 @@
+const STARTING_MISSILES: i32 = 8;
+const READY_AMOUNT: i32 = 2;
+
+// const (STARTING_MISSILES, READY_AMOUNT): (i32, i32 ) = (8, 2)
+
 fn main() {
-    println!("Hello, world!");
+    // // Regular Assignment
+    // let mut missiles: i32 = STARTING_MISSILES;
+    // let ready: i32 = READY_AMOUNT;
+    
+    // // Pattern Assignment
+    // I'm not sure but it seems
+    // that the variables are,
+    // both mutable by default
+    let (missiles, ready): (i32, i32) = (STARTING_MISSILES, READY_AMOUNT);
+    
+    println!("Firing {} of my {} missiles...", ready, missiles);
+
+    // // Base mutable variable redeclaration of values.
+    // missiles = missiles - ready;
+    // println!("{} missiles left",  missiles);
+
+    println!("{} missiles left",  missiles = missiles - ready);
+
+    let _unused_variable = 0;
 }
